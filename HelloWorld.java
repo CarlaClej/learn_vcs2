fb5670bf6efe225ef5ba706b6e61c1be7df3a9e2
CarlaClej <clej.carla@yahoo.ro>
2020-02-17 20:48:09 +0200

    added another new comment

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 	// some changes
 	// some changes2
+	// mai facem niste schimbari
 	public static void main(String[] args) {
 		System.out.println("Hello World! We are learning GIT");
 	}
