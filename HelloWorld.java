246bd81ee328df77e5e26896a1079e22fa722ff7
VargaCatalin <catavarga@yahoo.com>
2020-02-17 20:49:54 +0200

    i created a new comment

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
 	// some changes
 	// some changes2
 	// mai facem niste schimbari
+	// bineee 
 	public static void main(String[] args) {
 		System.out.println("Hello World! We are learning GIT");
 	}
